1cb36c83737875afb49fd2ed31f0835141ac6b4e
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-07-28 21:41:18 +0200

    Partie 1 - Functions OK.

--- Partie1/functions.php
+++ Partie1/functions.php
@@ -1,17 +1,18 @@
 <?php
 
-// Création d'un objet PDO.
-function getDataBase($dsn, $user, $password = '', $option = [])
-{
-    try {
-        // Récupération du DSN dans le fichier param.php
-        // $dsn = 'mysql:host=localhost;dbname=colyseum;charset=utf8;';
-        $db = new PDO($dsn, $user, $password, $option);
-        return $db;
+    // Création d'une instance PDO et définition du mode d'erreur.
+    function getDataBase($dsn, $user, $password = '', $option = [])
+    {
+        try {
+            // Récupération du DSN dans le fichier param.php.
+            // $dsn = 'mysql:host=localhost;dbname=colyseum;charset=utf8;';
+            $db = new PDO($dsn, $user, $password, $option);
+            return $db;
+        }
+        // Levage de l'exception.
+        catch (PDOException $e) {
+            // Appel du message.
+            die('Echec lors de la connexion à la base de données : ' . $e->getMessage());
+        }
     }
-    // Levage de l'exception.
-     catch (PDOException $e) {
-        // Appel du message.
-        die('Il y a un problème de connexion à la base de données : ' . $e->getMessage());
-    }
-}
+?>
