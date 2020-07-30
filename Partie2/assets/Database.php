39ae826d500dfb0c8f9f7cf0bc8a3b454a7b23cf
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-07-30 20:49:48 +0200

    Modification de Database et Param.

--- Partie2/assets/Database.php
+++ Partie2/assets/Database.php
@@ -1,20 +1,20 @@
 <?php
     class Database
     {
-        // Le mot clé "static" permet d'exécuter à une méthode de s'éxécuter sans avoir à instancier une class.
+        // Le mot clé "static" permet à une méthode de s'exécuter sans avoir à instancier une classe.
         public static function getInstance()
         {
             try{
-		    	$db = new PDO(DSN, USER, PWD);
-		    }
+                $db = new PDO(DSN, USER, PWD, ERR);
+            }
 		    // Levée de l'exception.
 		    catch(PDOException $e){
-	      		// Appel du message
+	      		// Appel du message.
 	      		die('Echec lors de la connexion à la base de données : '.$e->getMessage());
             }
-            
             return $db;
         }
     }
+    var_dump($db);
     var_dump(Database::getInstance());
 ?>
--- Partie2/assets/param.php
+++ Partie2/assets/param.php
@@ -3,5 +3,6 @@
 <?php
     define('USER', 'admin');
     define('PWD', 'L@uise19');
-    define('DSN', 'mysql:host=localhost;dbname=`hospitalE2N`;charset=utf8;');
+    define('DSN', 'mysql:host=localhost;dbname=hospitalE2N;charset=utf8');
+    define('ERR', '[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]');
 ?>
