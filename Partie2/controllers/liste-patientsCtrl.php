aa3313bd6692c463bcb202e4125bab1b68a5f498
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-08-17 22:38:15 +0200

    Mise à jour de liste-patientsCtrl.

--- Partie2/controllers/liste-patientsCtrl.php
+++ Partie2/controllers/liste-patientsCtrl.php
@@ -1,8 +1,10 @@
 <?php
 require_once dirname(__FILE__) . '/../models/Patient.php';
 
+// Instanciation d'un nouvel objet.
 $patient = new Patient();
 
+// Appel de la fonction realAll() qui affiche la liste de patients enregistrés.
 $patientsList = $patient->readAll();
 
 require_once dirname(__FILE__) . '/../views/liste-patients.php';
