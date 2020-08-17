58541a62cd827b2e829ada11100597e3eebc3d5f
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-08-17 21:55:42 +0200

    Mise à jour de la view ajout-patient.

--- Partie2/views/ajout-patient.php
+++ Partie2/views/ajout-patient.php
@@ -1,14 +1,19 @@
 <?php include_once dirname(__FILE__) . '/includes/header.php'; ?>
-<div class="container d-flex align-items-center justify-content-center bg-dark">
+
+<div class="container d-flex align-items-center justify-content-center">
+<!-- Il reste à centrer la card. -->
+
+    <!-- Affichage d'un message de succés de création de patient. -->
     <?php if(isset($createSuccess)): ?>
         <div class="alert alert-success" role="alert">
         <p>Patient ajouté !</p>
         </div>
     <?php endif; ?>
+
     <div class="p-3 rounded-lg shadow">
         <form action="" method="post">
             <fieldset class="p-3 border">
-                <legend>Nouveau patient</legend>
+                <legend class="font-weight-bold">Nouveau patient</legend>
                 <div class="w-100 d-sm-flex">
                     <div class="mr-2 w-100">
                         <label for="lastname">Nom de famille</label>
@@ -32,10 +37,11 @@
                     <input class="form-control" id="phone" type="tel" name="phone">
                 </div>
                 <div class="mt-3 w-100 d-flex justify-content-center">
-                    <button class="btn btn-info" type="submit" name="subscribe">Enregistrer</button>
+                    <button class="btn btn-success font-weight-bold" type="submit" name="subscribe">Enregistrer</button>
                 </div>
             </fieldset>
         </form>
     </div>
 </div>
+
 <?php include_once dirname(__FILE__) . '/includes/footer.php'; ?>
