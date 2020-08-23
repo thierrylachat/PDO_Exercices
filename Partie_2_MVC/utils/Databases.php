4499a3d9ee09a3c6727b585db9b37562fa3fecd3
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-08-23 21:28:32 +0200

    Partie 2 MVC mise à jour des fichiers utils, config et assets.

--- Partie_2_MVC/utils/Databases.php
+++ Partie_2_MVC/utils/Databases.php
@@ -1,19 +1,24 @@
 <?php
+
+// Création du chemin absolu vers les constantes de connexion.
 require_once dirname(__FILE__).'/../config/config.php';
+
 class Databases
 {
-    // static permet d'executer la fonction sans instance de class
+    // Le mot clé "static" permet à une méthode de s'exécuter sans avoir à instancier une classe.
     public static function getInstance()
     {
         try {
             $db = new PDO('mysql:host='.HOST.';dbname='.DB_NAME.';charset=utf8', USER, PASSWORD);
             $option = ERR;
         }
-        // lève l'exception
+
+        // Levée de l'exception.
         catch (PDOException $e) {
-            // appel du message
-            die('Il y a un problème de connexion à la base de données : ' . $e->getMessage());
+            // Appel du message.
+            die('Echec lors de la connexion à la base de données : '.$e->getMessage());
         }
+        
         return $db;
     }
 }
