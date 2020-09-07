e3f3cb7d84d04dcde688da74d54d352a5ada7dac
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-09-07 20:55:33 +0200

    PDO- TP2 Correction - Index.php et dossier includes OK.

--- PDO-TP2/index.php
+++ PDO-TP2/index.php
@@ -10,10 +10,13 @@
 </head>
 
 <body class="bg-dark text-white">
+    <!-- Insertion de la navbar dans les pages du site avec un include. -->
     <?php include (dirname(__FILE__).'/includes/menu.html')?>
     <h1 class="text-center mt-5" style="padding:17.5%;">PDO-TP2</h1>
 </body>
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </html>
