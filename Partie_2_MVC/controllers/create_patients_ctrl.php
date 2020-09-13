d1d30dae5829d05d59b61e6a5ed41f77589bb794
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-09-13 21:02:10 +0200

    PDO - Hospital - Verifies OK.

--- Partie_2_MVC/controllers/create_patients_ctrl.php
+++ Partie_2_MVC/controllers/create_patients_ctrl.php
@@ -1,6 +1,7 @@
 <?php
     require_once dirname(__FILE__).'/../model/Patients.php';
-    //validation des champs 
+    
+    // Validation des données utilisateurs.
     $isSubmitted = false;
     $regexName = "/^[A-Za-zéÉ][A-Za-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœ]+((-| )[A-Za-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœ]+)?$/";
     $regexDate = "/^((?:19|20)[0-9]{2})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/";
@@ -8,15 +9,23 @@
     $firstname = $lastname = $birthdate = $phone = $mail='';
     $errors = [];
 
+    // Soumission du formulaire.
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // Appel du fichier verifies qui contient les vérifications du formulaire.
         require_once dirname(__FILE__) . '/../controllers/verifies.php';
     }
 
+// Soumission du formulaire et vérification de l'absence d'erreurs.
 if ($isSubmitted && count($errors)== 0) {
+
+    // Instanciation d'un nouvel objet.
     $patients = new Patients (0, $_POST['firstname'],$_POST['lastname'],$_POST['birthdate'],$_POST['phone'],$_POST['mail']);
     
+    // Appel de la fonction create() du model qui va permettre de créer un nouveau patient.
     if($patients->create())
     {
+        // Affichage du message de succès de création de nouveau patient. 
         $createPatientsSuccess = true;
     }
 }
